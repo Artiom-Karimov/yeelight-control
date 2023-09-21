c70db24831f89c73cff94dc58a1c94d6a22f05e3
Artiom Karimov <lunatic0259@gmail.com>
2023-09-21 13:56:32 +0300

    Added simple telnet client

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { Yeelight } from './yeelight';
+import { TelnetConnection } from './utils/telnet-connection';
+import { PowerCommand } from './device/commands/implementations/power.command';
+import { Power } from './device/enums/power';
 
-export const start = () => {
-  const yeelight = new Yeelight();
+// export const start = () => {
+//   const yeelight = new Yeelight();
 
-  yeelight.on(
-    'discovery',
-    async (data) =>
-      await writeFile(
-        resolve(__dirname, '..', 'test-files', `${Date.now()}.json`),
-        JSON.stringify(data, undefined, 2),
-      ),
-  );
+//   yeelight.on(
+//     'discovery',
+//     async (data) =>
+//       await writeFile(
+//         resolve(__dirname, '..', 'test-files', `${Date.now()}.json`),
+//         JSON.stringify(data, undefined, 2),
+//       ),
+//   );
+// };
+const start = async () => {
+  const client = new TelnetConnection('192.168.88.23', 55443);
+  await client.connect();
+  console.log('Connect from main');
+  const command = new PowerCommand(null as any, Power.off);
+  client.send(command.data);
 };
 
 start();
